157e07469db4cc0125f16c30b1e783dd018e5e92
Dr Kim Foale <kim@gfsc.studio>
2019-06-19 15:29:10 +0100

    Seed trails

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var toggle = document.getElementById('js--navigation__toggle')
-var menu = document.getElementById('js--navigation__menu')
-
-function classToggle() {
-  menu.classList.toggle('navigation--hide')
-}
-
-toggle.addEventListener('click', classToggle)
-classToggle()
